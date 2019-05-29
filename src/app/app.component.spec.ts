594cb8306577ec8d6ee6b3e45049ba167301fe58
Dmitry Egorov (DevExpress) <egorov.dmitry@corp.devexpress.com>
2019-05-29 19:09:53 +0300

    LGTM

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DevExtremeModule } from 'devextreme-angular';
 import { LocalizeStubPipe, TranslateStubPipe } from './../../testing';
@@ -6,37 +6,33 @@ import { LocalizeStubPipe, TranslateStubPipe } from './../../testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        DevExtremeModule
-      ],
-      declarations: [
-        AppComponent,
-        LocalizeStubPipe,
-        TranslateStubPipe
-      ],
+      imports: [RouterTestingModule, DevExtremeModule],
+      declarations: [AppComponent, LocalizeStubPipe, TranslateStubPipe]
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-
-  it(`should render DxList`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should render DxList`, () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.dx-list')).toBeTruthy();
-  }));
+  });
 
-  it('should render DxToolbar', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+  it('should render DxToolbar', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.dx-toolbar')).toBeTruthy();
-  }));
+  });
 });
--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/toPromise';
 
 import { Order } from './../../models/order';
 
@@ -8,12 +7,13 @@ import { handleError } from './../services-utils';
 
 @Injectable()
 export class OrderService {
-    private ordersUrl = 'api/orders';
-    constructor(private http: HttpClient) { }
-    getOrders(): Promise<Order[]> {
-        return this.http.get(this.ordersUrl)
-            .toPromise()
-            .then(response => response as Order[])
-            .catch(handleError);
-    }
+  private ordersUrl = 'api/orders';
+  constructor(private http: HttpClient) {}
+  getOrders(): Promise<Order[]> {
+    return this.http
+      .get(this.ordersUrl)
+      .toPromise()
+      .then(response => response as Order[])
+      .catch(handleError);
+  }
 }
